72832ca365b0b81441d8213f694a7261cb5f5eb1
Tom Parker-Shemilt <palfrey@tevp.net>
2019-05-05 17:51:41 +0100

    Redo cron with new vulnerabilities code

--- notifier/cron.py
+++ notifier/cron.py
@@ -2,17 +2,9 @@ from .models import SlackRepoLink, SlackVulnerabilitySent
 from django.utils import timezone
 import datetime
 from .slack import session
-from .vulnerabilities import get_vulnerabilities, not_sent
+from .vulnerabilities import get_vulnerabilities, repo_send_for_link
 
 def update_slack():
     max_age = timezone.now() - datetime.timedelta(days=1)
     for link in SlackRepoLink.objects.all():
-        slack_session = session(instance=link.slack)
-        for v in not_sent(link):
-            message = f"{v.severity} vulnerability in <{v.repo.web_url()}|{v.repo.org.login}/{v.repo.name}> package {v.package} versions '{v.vulnerableVersions}' ('{v.requirements}' required in {v.manifest_path}) <{v.url}|{v.description}>"
-            res = slack_session.post("https://slack.com/api/chat.postMessage", json={
-                "channel": link.channel,
-                "text": message
-            })
-            res.raise_for_status()
-            SlackVulnerabilitySent(slack=link, vulnerability=v).save()
+        repo_send_for_link(github, link)
